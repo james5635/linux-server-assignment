e8d58f989dac5f1d6c7c725eb885873741fcd748
james5635 <brorojame2029@gmail.com>
2026-02-27 21:25:06 +0700

    update

--- src/mail_server/mail_server.sh
+++ src/mail_server/mail_server.sh
@@ -4,25 +4,6 @@ echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 dnf install -y postfix dovecot
 dnf clean all
 
-
-# Configure Postfix
-tee /etc/postfix/main.cf > /dev/null <<EOF
-myhostname = mail.example.com
-mydomain = example.com
-myorigin = \$mydomain
-inet_interfaces = all
-mydestination = \$myhostname, localhost.\$mydomain, localhost, \$mydomain
-mynetworks = 0.0.0.0/0
-home_mailbox = Maildir/
-smtpd_banner = \$myhostname ESMTP
-EOF
-
-# Configure Dovecot
-tee /etc/dovecot/conf.d/10-mail.conf > /dev/null <<EOF
-mail_location = maildir:~/Maildir
-EOF
-
-
 sed -i "s|ssl = required|ssl = no|" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s|ssl_cert = </etc/pki/dovecot/certs/dovecot.pem|ssl_cert = |" /etc/dovecot/conf.d/10-ssl.conf
 sed -i "s|ssl_key = </etc/pki/dovecot/private/dovecot.pem|ssl_key = |" /etc/dovecot/conf.d/10-ssl.conf
