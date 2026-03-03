2c2b8a3ff861a54d9e7ec30ff4fb48a3fefa4da6
james5635 <brorojame2029@gmail.com>
2026-03-03 15:30:20 +0700

    add docs pdf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,6 +10,23 @@ project = 'linux-server-assignment'
 copyright = '2026, Sou Chanrojame'
 author = 'Sou Chanrojame, Orn Pheakdey, Long Neron, Le Sreyma, Then Sivthean'
 
+# Fudge the latex author list, insert \and where you want the line break to appear
+max_characters_author_line = 50
+if len(author) > max_characters_author_line:
+    author_list = [author.strip() for author in author.split(',')]
+    i = len(author_list)
+    for i in range(1, len(author_list)):
+        if len(", ".join(author_list[:i])) > max_characters_author_line:
+            i = i-1
+            break
+    latex_author = ", ".join(author_list[:i]) + ", \\and " + ", ".join(author_list[i:])
+else:
+    latex_author = author
+
+latex_elements = {
+    "maketitle": "\\author{" + latex_author + "}\\sphinxmaketitle"
+}
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
