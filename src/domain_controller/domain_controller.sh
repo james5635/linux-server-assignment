0ace46e2eaaca06136fd3442318f8b1c5f2d70ab
james5635 <brorojame2029@gmail.com>
2026-04-05 15:14:46 +0700

    fix domain controller

--- src/domain_controller/domain_controller.sh
+++ src/domain_controller/domain_controller.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 
 # installing freeipa with docker
 # docker run -h ipa.example.test --read-only --cgroupns=host -v /sys/fs/cgroup:/sys/fs/cgroup:rw  -ti \
@@ -7,10 +6,9 @@ echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 #       freeipa/freeipa-server:centos-9-stream   ipa-server-install -U -r EXAMPLE.TEST --no-ntp --skip-mem-check
 
 # manually installing freeipa
+# echo "fastestmirror=True" >> /etc/dnf/dnf.conf
 # dnf -y install freeipa-server freeipa-server-dns 
 # ipa-server-install --setup-dns
-
-ipa-server-install -U -r EXAMPLE.TEST --no-ntp --skip-mem-check
 
 # for client
 # docker run  -it  --privileged --cgroupns=host --tmpfs=/run --tmpfs=/tmp --volume=/sys/fs/cgroup:/sys/fs/cgroup:rw trfore/docker-centos9-systemd:latest
